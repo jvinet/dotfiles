4821e0cd3a30beb6f088f18a22e577082f34ec49
Judd Vinet <judd@betsmartmedia.com>
2013-10-22 15:36:46 -0700

    new scripts/utils, updated xfce4 config

---
+++ cpupower.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+"""
+Listens for DBus signals when the computer goes on/off battery power,
+and adjusts CPU speed to save juice when necessary.
+"""
+
+from subprocess import call
+import dbus, gobject
+from dbus.mainloop.glib import DBusGMainLoop
+
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+bus = dbus.SessionBus()
+
+def listener(onBattery):
+    if onBattery:
+        call(['/usr/bin/cpuspeed', 'slow'])
+    else:
+        call(['/usr/bin/cpuspeed', 'fast'])
+
+bus.add_signal_receiver(listener,
+                        dbus_interface="org.freedesktop.PowerManagement",
+                        signal_name="OnBatteryChanged")
+
+loop = gobject.MainLoop()
+loop.run()

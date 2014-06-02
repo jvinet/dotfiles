e6507d0c87145c49aee22a5db5cf6f8c5418140b
Judd Vinet <judd@betsmartmedia.com>
2014-06-02 10:50:30 -0700

    gimme back my systray

--- screenlayout/single.sh
+++ screenlayout/single.sh
@@ -9,7 +9,7 @@ xrandr \
 	--output DP1 --off \
 	--output HDMI2 --off \
 	--output HDMI1 --off \
-	--output eDP1 --mode 1366x768 --pos 0x0 --rotate normal \
+	--output eDP1 --mode 1366x768 --pos 0x0 --rotate normal --primary \
 	--output $DISP --off \
 	--output VGA1 --off
 

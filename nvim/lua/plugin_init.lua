3558511778dd0e47426ff64259957b774c422080
Judd Vinet <jvinet@gmail.com>
2023-02-17 14:28:40 -0800

    sumneko_lua -> lua_ls

--- nvim/lua/plugin_init.lua
+++ nvim/lua/plugin_init.lua
@@ -297,7 +297,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'elixirls', 'rust_analyzer', 'pylsp', 'tsserver', 'sumneko_lua', 'gopls', 'nimls' }
+local servers = { 'clangd', 'elixirls', 'rust_analyzer', 'pylsp', 'tsserver', 'lua_ls', 'gopls', 'nimls' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {

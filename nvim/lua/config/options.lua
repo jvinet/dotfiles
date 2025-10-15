d8a583c51cd7c7e29e77e0228bea14bdef61c2fa
Judd Vinet <jvinet@gmail.com>
2025-10-15 14:36:43 -0700

    Do not trust lsp to find the root, it is not wise

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,4 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.root_spec = { ".git", "cwd" }
 vim.g.snacks_animate = false
+
+vim.opt.relativenumber = false

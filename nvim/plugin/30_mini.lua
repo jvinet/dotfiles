9690b26df5d6581a3f09b36d81a73d99bac13f24
Judd Vinet <jvinet@zeroflux.org>
2026-04-13 09:46:58 -0700

    mini.files - window width adjustments

--- nvim/plugin/30_mini.lua
+++ nvim/plugin/30_mini.lua
@@ -220,7 +220,13 @@ end)
 -- - `:h MiniFiles-examples` - examples of common setups
 now_if_args(function()
   -- Enable directory/file preview
-  require('mini.files').setup({ windows = { preview = true } })
+  require('mini.files').setup({
+    windows = {
+      preview = true,
+      width_focus = 30,
+      width_preview = 70
+    }
+  })
 
   -- Add common bookmarks for every explorer. Example usage inside explorer:
   -- - `'c` to navigate into your config directory
@@ -232,35 +238,6 @@ now_if_args(function()
     MiniFiles.set_bookmark('w', vim.fn.getcwd, { desc = 'Working directory' })
   end
   Config.new_autocmd('User', 'MiniFilesExplorerOpen', add_marks, 'Add bookmarks')
-
-  local map_split = function(buf_id, lhs, direction)
-    local rhs = function()
-      -- Make new window and set it as target
-      local cur_target = MiniFiles.get_explorer_state().target_window
-      local new_target = vim.api.nvim_win_call(cur_target, function()
-        vim.cmd(direction .. ' split')
-        return vim.api.nvim_get_current_win()
-      end)
-
-      MiniFiles.set_target_window(new_target)
-      MiniFiles.go_in({ close_on_file = true })
-    end
-
-    -- Adding `desc` will result into `show_help` entries
-    local desc = 'Split ' .. direction
-    vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
-  end
-
-  vim.api.nvim_create_autocmd('User', {
-    pattern = 'MiniFilesBufferCreate',
-    callback = function(args)
-      local buf_id = args.data.buf_id
-      -- Tweak keys to your liking
-      map_split(buf_id, '<C-s>', 'belowright horizontal')
-      map_split(buf_id, '<C-v>', 'belowright vertical')
-      map_split(buf_id, '<C-t>', 'tab')
-    end,
-  })
 end)
 
 -- Miscellaneous small but useful functions. Example usage:

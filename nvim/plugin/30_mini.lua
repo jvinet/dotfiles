8c3647eacf5d09440a4ca11bbb94942cda2236c4
Judd Vinet <jvinet@zeroflux.org>
2026-04-14 14:27:43 -0700

    Re-add the C-s/v/t shortcuts for mini.files

--- nvim/plugin/30_mini.lua
+++ nvim/plugin/30_mini.lua
@@ -238,6 +238,35 @@ now_if_args(function()
     MiniFiles.set_bookmark('w', vim.fn.getcwd, { desc = 'Working directory' })
   end
   Config.new_autocmd('User', 'MiniFilesExplorerOpen', add_marks, 'Add bookmarks')
+
+  local map_split = function(buf_id, lhs, direction)
+    local rhs = function()
+      -- Make new window and set it as target
+      local cur_target = MiniFiles.get_explorer_state().target_window
+      local new_target = vim.api.nvim_win_call(cur_target, function()
+        vim.cmd(direction .. ' split')
+        return vim.api.nvim_get_current_win()
+      end)
+
+      MiniFiles.set_target_window(new_target)
+      MiniFiles.go_in({ close_on_file = true })
+    end
+
+    -- Adding `desc` will result into `show_help` entries
+    local desc = 'Split ' .. direction
+    vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+  end
+
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'MiniFilesBufferCreate',
+    callback = function(args)
+      local buf_id = args.data.buf_id
+      -- Tweak keys to your liking
+      map_split(buf_id, '<C-s>', 'belowright horizontal')
+      map_split(buf_id, '<C-v>', 'belowright vertical')
+      map_split(buf_id, '<C-t>', 'tab')
+    end,
+  })
 end)
 
 -- Miscellaneous small but useful functions. Example usage:

e986e182c0a5ef3ea5f9828d5a54ba61c4b77350
Judd Vinet <jvinet@zeroflux.org>
2026-09-25 10:44:15 -0700

    tmux: N to leave a temp note on a pane

--- nvim/plugin/30_mini.lua
+++ nvim/plugin/30_mini.lua
@@ -113,7 +113,7 @@ now(function() require('mini.sessions').setup() end)
 -- See also:
 -- - `:h MiniStarter-example-config` - non-default config examples
 -- - `:h MiniStarter-lifecycle` - how to work with Starter buffer
-now(function() require('mini.starter').setup() end)
+now(function() require('mini.starter').setup({ autoopen = false, }) end)
 
 -- Statusline. Sets `:h 'statusline'` to show more info in a line below window.
 -- Example usage:
